ef7503bcf93786722efd79d2a30a7eeb7ea5f53c
clicktx <clicktx@gmail.com>
2016-11-04 09:29:11 +0900

    implement core code

--- lib/Mojolicious/Plugin/Argon2.pm
+++ lib/Mojolicious/Plugin/Argon2.pm
@@ -1,10 +1,41 @@
 package Mojolicious::Plugin::Argon2;
 use Mojo::Base 'Mojolicious::Plugin';
+use Crypt::Argon2 qw/argon2i_pass argon2i_verify/;
 
 our $VERSION = '0.01';
 
 sub register {
-    my ( $self, $app ) = @_;
+    my ( $self, $app, $conf ) = @_;
+    my $t_cost      = $conf->{t_cost}      || 3;
+    my $m_factor    = $conf->{m_factor}    || '32M';
+    my $parallelism = $conf->{parallelism} || 1;
+    my $tag_size    = $conf->{tag_size}    || 16;
+
+    $app->helper(
+        argon2 => sub {
+            my $c = shift;
+            my ( $secret, $salt ) = ( shift, shift || _salt() );
+
+            return argon2i_pass( $secret, $salt, $t_cost, $m_factor,
+                $parallelism, $tag_size );
+        }
+    );
+
+    $app->helper(
+        argon2_verify => sub {
+            my $c = shift;
+            my ( $encoded, $plain ) = @_;
+
+            return argon2i_verify( $encoded, $plain );
+        }
+    );
+}
+
+# This code from Mojolicious::Plugin::Bcrypt::_salt
+sub _salt {
+    my $num = 999999;
+    my $cr = crypt( rand($num), rand($num) ) . crypt( rand($num), rand($num) );
+    substr( $cr, 4, 20 );
 }
 
 1;
